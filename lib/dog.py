48c6dfc3622a7f84bdb1082a31a56b21f2848a48
whiplade <nmawira13@gmail.com>
2023-08-22 08:12:02 +0300

    Completed Assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+
+fido = Dog("Fido", "German Shepherd")
+print(fido.breed)
